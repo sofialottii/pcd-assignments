5724e264070e53e041682df879f9cc5704867329
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2026-04-18 12:03:21 +0200

    sorry I forgot this file

--- assignment-01/src/pcd/ass01/thread_version/model/board/BoardConf.java
+++ assignment-01/src/pcd/ass01/thread_version/model/board/BoardConf.java
@@ -1,9 +1,9 @@
 package pcd.ass01.thread_version.model.board;
 
+import pcd.ass01.thread_version.model.ball.BotBall;
 import pcd.ass01.thread_version.model.ball.PlayerBall;
 import pcd.ass01.thread_version.model.ball.SmallBall;
-import pcd.sketch01.Ball;
-import pcd.sketch01.Boundary;
+
 
 import java.util.List;
 
@@ -12,6 +12,9 @@ public interface BoardConf {
 	Boundary getBoardBoundary();
 	
 	PlayerBall getPlayerBall();
+	BotBall getBotBall();
 	
 	List<SmallBall> getSmallBalls();
+
+
 }
