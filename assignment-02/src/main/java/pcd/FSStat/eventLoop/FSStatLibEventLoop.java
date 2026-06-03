0636b25061100ab17cca90eb96d6b22929ee1d94
Sofia <irenesofia.lotti@studio.unibo.it>
2026-06-03 13:02:46 +0200

    feat(WIP): configured vertx ambient

--- assignment-02/src/main/java/pcd/FSStat/eventLoop/FSStatLibEventLoop.java
+++ assignment-02/src/main/java/pcd/FSStat/eventLoop/FSStatLibEventLoop.java
@@ -1,4 +1,13 @@
 package pcd.FSStat.eventLoop;
 
+import io.vertx.core.*;
+
 public class FSStatLibEventLoop {
+
+    public static void main(String[] args) {
+        System.out.println("miao");
+        Vertx vertx = Vertx.vertx();
+
+    }
+
 }
