52bca4353ad5f44c53f38a21e4298fd017cc8e6f
Margherita Zanchini <margherita.zanchini@studio.unibo.it>
2026-06-06 15:03:54 +0200

    fix: report now is complted at the right time

--- assignment-02/src/main/java/pcd/FSStat/eventLoop/FSStatLib.java
+++ assignment-02/src/main/java/pcd/FSStat/eventLoop/FSStatLib.java
@@ -1,11 +1,13 @@
 package pcd.FSStat.eventLoop;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.file.FileProps;
 import io.vertx.core.file.FileSystem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,6 +22,7 @@ public class FSStatLib {
 
     public Future<Report> getFSReport(String dir, long maxFS, int nb) {
 
+        Promise<Report> promise = Promise.promise();
         Report report = new Report(maxFS, nb);
         //TODO: gestire i report nel caso ricorsivo
 
@@ -28,28 +31,32 @@ public class FSStatLib {
 
         fileList.onComplete((AsyncResult<List<String>> res) -> {
 
+            List<Future<?>> futures = new ArrayList<>();
+
             for (String file : res.result()) {  //iteriamo i file
 
                 Future<FileProps> fileProps = fs.props(file); //leggiamo le proprietà del file
+                futures.add(fileProps);
 
                 fileProps.onComplete((AsyncResult<FileProps> res2) -> {
                     report.addFile(res2.result().size());
-                    System.out.println(res2.result().size());
-
+                    //System.out.println(res2.result().size());
 
                     /*if (res2.result().isDirectory()) { //se il file è una directory: ricorsione
                         getFSReport(file, maxFS, nb);
                     } else { //if (res2.result().isRegularFile()) { //se è un file: chiama l'addFile
                         report.addFile(res2.result().size());
                     }*/
-
                 });
             }
 
+            Future.all(futures)
+                    .onSuccess(r -> {
+                        promise.complete(report);
+                    });
         });
 
-        Promise<Report> promise = Promise.promise();
-        promise.complete(report);
+
         return promise.future();
         //NON ASPETTA: da fixare. Bisogna fare in modo che aspetti
 
