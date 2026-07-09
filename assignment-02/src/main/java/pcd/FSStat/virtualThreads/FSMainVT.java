395a253a1e31bef2c239a4fd1773d0d3ebf295d6
Sofia <irenesofia.lotti@studio.unibo.it>
2026-07-09 12:43:44 +0200

    feat: implement virtualThreads task

--- assignment-02/src/main/java/pcd/FSStat/virtualThreads/FSMainVT.java
+++ assignment-02/src/main/java/pcd/FSStat/virtualThreads/FSMainVT.java
@@ -1,4 +1,14 @@
 package pcd.FSStat.virtualThreads;
 
 public class FSMainVT {
+    public static void main(String[] args) {
+
+        final String dir = "./";
+        final long maxFS = 1000;
+        final int nb = 10;
+
+        FSStatLibVT lib = new FSStatLibVT();
+
+        System.out.println(lib.getFSReport(dir, maxFS, nb).toString());
+    }
 }
--- assignment-02/src/main/java/pcd/FSStat/virtualThreads/FSStatLibVT.java
+++ assignment-02/src/main/java/pcd/FSStat/virtualThreads/FSStatLibVT.java
@@ -1,4 +1,63 @@
 package pcd.FSStat.virtualThreads;
 
+import pcd.FSStat.common.Report;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class FSStatLibVT {
+
+    public Report getFSReport(String dir, long maxFS, int nb) {
+
+        Report report = new Report(maxFS, nb);
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            try {
+                recursiveFileSearch(dir, report, executor);
+            } finally {
+                executor.shutdown();
+            }
+
+        }
+
+        return report;
+    }
+
+    private void recursiveFileSearch(String dirPath, Report report, ExecutorService executor) {
+
+        File dir = new File(dirPath);
+        File[] directoryListing = dir.listFiles();
+
+        List<Future<?>> futures = new ArrayList<>();
+
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                if (child.isDirectory()) {
+                    Future<?> future = executor.submit(() -> {
+                        recursiveFileSearch(child.getAbsolutePath(), report, executor);
+                    });
+                    futures.add(future);
+                }
+
+                else if (child.isFile()) {
+                    report.addFile(child.length());
+                }
+            }
+        } else {
+            throw new RuntimeException();
+        }
+
+        for (Future<?> task : futures) {
+            try {
+                task.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
 }
